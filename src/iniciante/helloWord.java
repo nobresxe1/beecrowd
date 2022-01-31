a0582a6cc3a458f9c08743e24c1300ad56ca48e9
Rodrigo Garcia da Silva <rgs190385@gmail.com>
2022-01-30 22:11:26 -0300

    Resolvendo exercicios do beecrowd desde o inicio

---
+++ src/iniciante/helloWord.java
@@ -0,0 +1,14 @@
+package iniciante;
+import java.io.IOException;
+
+
+public class helloWord {
+
+       /* public static void main(String[] args) throws IOException {
+
+            System.out.println("Hello World!");
+
+        }*/
+
+
+}
